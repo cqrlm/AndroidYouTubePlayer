81951d8a3d0e48a0b808f356c5e65bebf8149bca
cqrlm <conqueor@yandex.ru>
2019-12-11 06:59:45 +0300

    Revert "Retrofit support added"
    This reverts commit e591911d

--- app/src/main/java/com/example/lab6_youtube_app/DeveloperKey.kt
+++ app/src/main/java/com/example/lab6_youtube_app/DeveloperKey.kt
@@ -2,6 +2,6 @@ package com.example.lab6_youtube_app
 
 class DeveloperKey {
     companion object {
-        const val DEVELOPER_KEY = BuildConfig.YOUTUBE_API_KEY
+        const val DEVELOPER_KEY = "AIzaSyDOMzfrGF_XBgjvvfa_uXELGrOKYF5DV-4"
     }
 }
