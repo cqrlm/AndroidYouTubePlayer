823cc41507bbe02f077c65e743821c21df709048
cqrlm <conqueor@yandex.ru>
2019-12-11 05:39:11 +0300

    Retrofit support added

--- app/src/main/java/com/example/lab6_youtube_app/recyclerview/RecyclerAdapter.kt
+++ app/src/main/java/com/example/lab6_youtube_app/recyclerview/RecyclerAdapter.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
@@ -15,21 +16,28 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.lab6_youtube_app.DeveloperKey
 import com.example.lab6_youtube_app.R
 import com.google.android.youtube.player.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class RecyclerAdapter(ctx: Context, val videos: List<VideoItem>) :
+
+class RecyclerAdapter(ctx: Context, val youtubeVideos: List<YoutubeVideo>) :
     RecyclerView.Adapter<RecyclerAdapter.VideoInfoHolder>() {
 
     inner class VideoInfoHolder(itemView: View) : ViewHolder(itemView) {
         val containerYouTubePlayer: FrameLayout =
             itemView.findViewById(R.id.youtube_holder) as FrameLayout
-        val relativeLayoutOverYouTubeThumbnailView: RelativeLayout =
-            itemView.findViewById<View>(R.id.relativeLayout_over_youtube_thumbnail) as RelativeLayout
+        val youTubeFrameLayout: RelativeLayout =
+            itemView.findViewById<View>(R.id.youtube_frame_layout) as RelativeLayout
         val youTubeThumbnailView: YouTubeThumbnailView =
             itemView.findViewById<View>(R.id.youtube_thumbnail) as YouTubeThumbnailView
         val playButton: ImageView =
             itemView.findViewById<View>(R.id.btn_youtube_player) as ImageView
+        val videoTitle: TextView =
+            itemView.findViewById(R.id.video_title) as TextView
     }
 
+    val apiService = VideoService.create()
     lateinit var youTubePlayer: YouTubePlayer
     private lateinit var youTubePlayerFragment: YouTubePlayerSupportFragment
     private val fragmentManager = (ctx as AppCompatActivity).supportFragmentManager
@@ -48,42 +56,7 @@ class RecyclerAdapter(ctx: Context, val videos: List<VideoItem>) :
         holder: VideoInfoHolder,
         position: Int
     ) {
-
-        val onThumbnailLoadedListener: YouTubeThumbnailLoader.OnThumbnailLoadedListener =
-            object : YouTubeThumbnailLoader.OnThumbnailLoadedListener {
-                override fun onThumbnailError(
-                    youTubeThumbnailView: YouTubeThumbnailView?,
-                    errorReason: YouTubeThumbnailLoader.ErrorReason?
-                ) {
-                    Log.d("ThumbnailError", errorReason as String)
-                }
-
-                override fun onThumbnailLoaded(
-                    youTubeThumbnailView: YouTubeThumbnailView,
-                    strng: String?
-                ) {
-                    youTubeThumbnailView.visibility = View.VISIBLE
-                    holder.relativeLayoutOverYouTubeThumbnailView.visibility = View.VISIBLE
-                }
-            }
-        holder.youTubeThumbnailView.initialize(
-            DeveloperKey.DEVELOPER_KEY,
-            object : YouTubeThumbnailView.OnInitializedListener {
-                override fun onInitializationSuccess(
-                    youTubeThumbnailView: YouTubeThumbnailView?,
-                    youTubeThumbnailLoader: YouTubeThumbnailLoader
-                ) {
-                    youTubeThumbnailLoader.setVideo(videos[position].id)
-                    youTubeThumbnailLoader.setOnThumbnailLoadedListener(onThumbnailLoadedListener)
-                }
-
-                override fun onInitializationFailure(
-                    youTubeThumbnailView: YouTubeThumbnailView?,
-                    youTubeInitializationResult: YouTubeInitializationResult?
-                ) {
-                    Log.d("InitializationFailure", youTubeInitializationResult as String)
-                }
-            })
+        initYouTubeThumbnailView(holder, position)
         holder.playButton.setOnClickListener {
             holder.containerYouTubePlayer.id = position + 1
 
@@ -114,42 +87,98 @@ class RecyclerAdapter(ctx: Context, val videos: List<VideoItem>) :
                     youTubePlayerFragment as Fragment
                 )
                 .commit()
-
-            var check = false
-            youTubePlayerFragment.initialize(
-                DeveloperKey.DEVELOPER_KEY,
-                object : YouTubePlayer.OnInitializedListener {
-                    override fun onInitializationSuccess(
-                        provider: YouTubePlayer.Provider?,
-                        player: YouTubePlayer,
-                        wasRestored: Boolean
-                    ) {
-                        if (!wasRestored) {
-                            youTubePlayer = player
-                            youTubePlayer.setOnFullscreenListener {
-                                if (!check) {
-                                    check = true
-                                    youTubePlayer.setFullscreen(check)
-                                    youTubePlayer.cueVideo(videos[position].id)
-                                }
-                            }
-                            youTubePlayer.fullscreenControlFlags = 0
-                            youTubePlayer.cueVideo(videos[position].id)
-//                            player.cuePlaylist("PLWz5rJ2EKKc8jQfNAUu5reIGFNNqpn26X")
-                        }
-                    }
-
-                    override fun onInitializationFailure(
-                        provider: YouTubePlayer.Provider?,
-                        youTubeInitializationResult: YouTubeInitializationResult?
-                    ) {
-                        Log.d("InitializationFailure", youTubeInitializationResult as String)
-                    }
-                })
+            initYouTubePlayerFragment(position)
         }
     }
 
     override fun getItemCount(): Int {
-        return videos.size
+        return youtubeVideos.size
+    }
+
+    private fun initYouTubeThumbnailView(holder: VideoInfoHolder, position: Int) {
+        holder.youTubeThumbnailView.initialize(
+            DeveloperKey.DEVELOPER_KEY,
+            object : YouTubeThumbnailView.OnInitializedListener {
+                override fun onInitializationSuccess(
+                    youTubeThumbnailView: YouTubeThumbnailView?,
+                    youTubeThumbnailLoader: YouTubeThumbnailLoader
+                ) {
+                    youTubeThumbnailLoader.setVideo(youtubeVideos[position].id)
+                    youTubeThumbnailLoader.setOnThumbnailLoadedListener(object :
+                        YouTubeThumbnailLoader.OnThumbnailLoadedListener {
+                        override fun onThumbnailError(
+                            youTubeThumbnailView: YouTubeThumbnailView?,
+                            errorReason: YouTubeThumbnailLoader.ErrorReason?
+                        ) {
+                            Log.d("ThumbnailError", errorReason as String)
+                        }
+
+                        override fun onThumbnailLoaded(
+                            youTubeThumbnailView: YouTubeThumbnailView,
+                            strng: String?
+                        ) {
+                            youTubeThumbnailView.visibility = View.VISIBLE
+                            holder.youTubeFrameLayout.visibility = View.VISIBLE
+                        }
+                    })
+                }
+
+                override fun onInitializationFailure(
+                    youTubeThumbnailView: YouTubeThumbnailView?,
+                    youTubeInitializationResult: YouTubeInitializationResult?
+                ) {
+                    Log.d("InitializationFailure", youTubeInitializationResult as String)
+                }
+            })
+    }
+
+    private fun initYouTubePlayerFragment(position: Int) {
+        var fullscreenCheck = false
+        youTubePlayerFragment.initialize(
+            DeveloperKey.DEVELOPER_KEY,
+            object : YouTubePlayer.OnInitializedListener {
+                override fun onInitializationSuccess(
+                    provider: YouTubePlayer.Provider?,
+                    player: YouTubePlayer,
+                    wasRestored: Boolean
+                ) {
+                    if (!wasRestored) {
+                        youTubePlayer = player
+                        youTubePlayer.setOnFullscreenListener {
+                            if (!fullscreenCheck) {
+                                fullscreenCheck = true
+                                youTubePlayer.setFullscreen(fullscreenCheck)
+                                youTubePlayer.cueVideo(youtubeVideos[position].id)
+                            }
+                        }
+                        youTubePlayer.fullscreenControlFlags = 0
+//                        youTubePlayer.cueVideo(response.body()?.item?.first()?.id)
+
+                        apiService.search(youtubeVideos[position].id)
+                            .enqueue(object : Callback<YoutubeVideoResponse> {
+                                override fun onFailure(
+                                    call: Call<YoutubeVideoResponse>,
+                                    t: Throwable
+                                ) {
+                                    // TODO
+                                }
+
+                                override fun onResponse(
+                                    call: Call<YoutubeVideoResponse>,
+                                    response: Response<YoutubeVideoResponse>
+                                ) {
+                                    youTubePlayer.cueVideo(response.body()?.item?.first()?.id)
+                                }
+                            })
+                    }
+                }
+
+                override fun onInitializationFailure(
+                    provider: YouTubePlayer.Provider?,
+                    youTubeInitializationResult: YouTubeInitializationResult?
+                ) {
+                    Log.d("InitializationFailure", youTubeInitializationResult as String)
+                }
+            })
     }
 }
--- app/src/main/java/com/example/lab6_youtube_app/recyclerview/VideoItem.kt
+++
@@ -1,3 +0,0 @@
-package com.example.lab6_youtube_app.recyclerview
-
-data class VideoItem(val id: String)
